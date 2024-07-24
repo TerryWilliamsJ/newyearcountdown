a00a232a855cd5648ab2e73826063800b8894eb4
TerryWilliamsJ <157618510+TerryWilliamsJ@users.noreply.github.com>
2024-07-24 22:29:50 +0530

    deploy

---
+++ script.js
@@ -0,0 +1,26 @@
+const countdown = () => {
+  const countDate = new Date("Jan 1, 2025 00:00:00").getTime();
+  const now = new Date().getTime();
+  const gap = countDate - now;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(gap / day);
+  const hours = Math.floor((gap % day) / hour);
+  const minutes = Math.floor((gap % hour) / minute);
+  const seconds = Math.floor((gap % minute) / second);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+
+  if (gap < 0) {
+      document.getElementById("countdown").innerHTML = "Happy New Year!";
+  }
+};
+
+setInterval(countdown, 1000);
